7eb6002da6cf8531e8d42fa4ef97fe788b1afc9c
Justas Dabrila <justasdabrila@gmail.com>
2018-09-24 13:45:16 +0000

    refreshToken: update to 2.0.0

--- src/refreshToken.js
+++ src/refreshToken.js
@@ -4,9 +4,13 @@ require("dotenv").load();
 
 const asyncCtx = async () => {
     const key = await roll20auth.getSessionKey(process.env.ROLL20_USERNAME, process.env.ROLL20_PASSWORD);
-    const gntkn = await roll20auth.getGNTKN(key, process.env.ROLL20_CAMPAIGN_ID);
+    const data = await roll20auth.getCampaignData(key, process.env.ROLL20_CAMPAIGN_ID);
 
-    fs.writeFile(".env.gntkn", `ROLL20_GNTKN=${gntkn}`, "utf8", console.log);
+    fs.writeFile(".env.gntkn", `
+ROLL20_GNTKN="${data.getGNTKN()}"
+ROLL20_CAMPAIGN_PATH="${data.getCampaignStoragePath()}"
+ROLL20_PLAYER_ID="${data.getPlayerId()}"
+`, "utf8", console.log);
     console.log("done.");
 };
 
